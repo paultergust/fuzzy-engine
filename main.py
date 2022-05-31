e08cbdfc02851ebf1bd4806def19945f365aeeb8
Paulo Moreira <prmoreira98@gmail.com>
2022-05-31 07:51:37 -0300

    Remove useless imports

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from datetime import datetime as dt
 
 def by_a_million(num):
     return num / 1_000_000
